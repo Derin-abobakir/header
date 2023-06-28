b50fdf87d1c6be7cbb9862878a8cb1754c36ffa3
Derin-abobakir <135045934+Derin-abobakir@users.noreply.github.com>
2023-06-29 01:34:11 +0300

    Add files via upload

---
+++ derin/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main</title>
+    <link rel="stylesheet" href="headers.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Geologica:wght@100&family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fa fa-bars"></i>
+        </label>
+        <label class="logo">Razga Company</label>
+        <ul>
+            <li><a class="home" href="#">Home</a></li>
+            <li><a class="login" href="#">Login</a></li>
+            <li><a class="jadwal" href="#">Jadwal</a></li>
+            <li><a class="dawakrdn" href="#">Dawakrdn</a></li>
+            <li><a class="bashakan" href="#">Bashakan</a></li>
+            <li><a class="about" href="#">About</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
